30122264ae598f453f80520a14ef78b5d3129366
Altman13 <consent@71gmail.com>
2025-11-10 13:28:34 +0300

    мелкие правки

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module aviation-booking
 
-go 1.23.0
+go 1.24.0
 
 toolchain go1.24.4
 
@@ -47,7 +47,7 @@ require (
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
-	golang.org/x/text v0.26.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- testdata/generator.go
+++ testdata/generator.go
@@ -1,108 +1,222 @@
 package testdata
 
 import (
+	"aviation-booking/models"
 	"fmt"
 	"math/rand"
 	"time"
 
-	"aviation-booking/models"
-
-	"github.com/bxcodec/faker/v3"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
-type TestDataGenerator struct{}
-
-func NewTestDataGenerator() *TestDataGenerator {
-	rand.Seed(time.Now().UnixNano())
-	return &TestDataGenerator{}
+type TestDataGenerator struct {
+	random *rand.Rand
 }
 
-// GenerateUsers генерирует тестовых пользователей
+func NewTestDataGenerator() *TestDataGenerator {
+	return &TestDataGenerator{
+		random: rand.New(rand.NewSource(time.Now().UnixNano())),
+	}
+}
+
+// GenerateUsers - генерация реалистичных пользователей
 func (g *TestDataGenerator) GenerateUsers(count int) ([]models.User, error) {
-	var users []models.User
+	users := make([]models.User, count)
+
+	// Русские имена и фамилии
+	firstNames := []string{
+		"Александр", "Алексей", "Андрей", "Антон", "Артем", "Борис", "Вадим", "Валентин",
+		"Валерий", "Виктор", "Виталий", "Владимир", "Вячеслав", "Геннадий", "Георгий",
+		"Денис", "Дмитрий", "Евгений", "Иван", "Игорь", "Кирилл", "Константин", "Максим",
+		"Михаил", "Никита", "Николай", "Олег", "Павел", "Петр", "Роман", "Сергей", "Станислав",
+		"Степан", "Юрий", "Ярослав",
+		"Александра", "Алена", "Алина", "Алла", "Анастасия", "Анна", "Валентина", "Валерия",
+		"Вера", "Виктория", "Галина", "Дарья", "Евгения", "Екатерина", "Елена", "Ирина",
+		"Ксения", "Лариса", "Любовь", "Людмила", "Марина", "Мария", "Надежда", "Наталья",
+		"Оксана", "Ольга", "Светлана", "Татьяна", "Юлия", "Яна",
+	}
+
+	lastNames := []string{
+		"Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов", "Попов", "Лебедев", "Козлов",
+		"Новиков", "Морозов", "Волков", "Соловьев", "Васильев", "Зайцев", "Павлов",
+		"Семенов", "Голубев", "Виноградов", "Богданов", "Воробьев", "Федоров", "Михайлов",
+		"Беляев", "Тарасов", "Белов", "Комаров", "Орлов", "Киселев", "Макаров", "Андреев",
+		"Ковалев", "Ильин", "Гусев", "Титов", "Кузьмин", "Кудрявцев", "Баранов", "Куликов",
+		"Алексеев", "Степанов", "Яковлев", "Сорокин", "Сергеев", "Романов", "Захаров",
+		"Борисов", "Королев", "Герасимов", "Пономарев", "Григорьев",
+		"Иванова", "Петрова", "Сидорова", "Смирнова", "Кузнецова", "Попова", "Лебедева",
+		"Козлова", "Новикова", "Морозова", "Волкова", "Соловьева", "Васильева", "Зайцева",
+		"Павлова", "Семенова", "Голубева", "Виноградова", "Богданова", "Воробьева",
+	}
+
+	domains := []string{"gmail.com", "yandex.ru", "mail.ru", "rambler.ru", "outlook.com", "yahoo.com"}
 
 	for i := 0; i < count; i++ {
-		var user models.User
-		err := faker.FakeData(&user)
-		if err != nil {
-			return nil, err
+		firstName := firstNames[g.random.Intn(len(firstNames))]
+		lastName := lastNames[g.random.Intn(len(lastNames))]
+		domain := domains[g.random.Intn(len(domains))]
+
+		// Создаем email на основе имени и фамилии
+		email := fmt.Sprintf("%s.%s%d@%s",
+			transliterate(firstName),
+			transliterate(lastName),
+			g.random.Intn(1000),
+			domain)
+
+		// Нормализуем имена (первая буква заглавная, остальные строчные)
+		caser := cases.Title(language.Russian)
+		normalizedFirstName := caser.String(firstName)
+		normalizedLastName := caser.String(lastName)
+
+		users[i] = models.User{
+			FirstName: normalizedFirstName,
+			LastName:  normalizedLastName,
+			Email:     email,
+			CreatedAt: time.Now().Add(-time.Duration(g.random.Intn(365)) * 24 * time.Hour),
+			UpdatedAt: time.Now(),
 		}
-
-		user.ID = uint(i + 1)
-		user.CreatedAt = time.Now()
-		user.UpdatedAt = time.Now()
-
-		users = append(users, user)
 	}
 
 	return users, nil
 }
 
-// GenerateAirports генерирует тестовые аэропорты
+// transliterate - транслитерация кириллицы в латиницу для email
+func transliterate(text string) string {
+	translitMap := map[rune]string{
+		'а': "a", 'б': "b", 'в': "v", 'г': "g", 'д': "d", 'е': "e", 'ё': "e", 'ж': "zh",
+		'з': "z", 'и': "i", 'й': "y", 'к': "k", 'л': "l", 'м': "m", 'н': "n", 'о': "o",
+		'п': "p", 'р': "r", 'с': "s", 'т': "t", 'у': "u", 'ф': "f", 'х': "kh", 'ц': "ts",
+		'ч': "ch", 'ш': "sh", 'щ': "shch", 'ъ': "", 'ы': "y", 'ь': "", 'э': "e", 'ю': "yu",
+		'я': "ya",
+		'А': "a", 'Б': "b", 'В': "v", 'Г': "g", 'Д': "d", 'Е': "e", 'Ё': "e", 'Ж': "zh",
+		'З': "z", 'И': "i", 'Й': "y", 'К': "k", 'Л': "l", 'М': "m", 'Н': "n", 'О': "o",
+		'П': "p", 'Р': "r", 'С': "s", 'Т': "t", 'У': "u", 'Ф': "f", 'Х': "kh", 'Ц': "ts",
+		'Ч': "ch", 'Ш': "sh", 'Щ': "shch", 'Ъ': "", 'Ы': "y", 'Ь': "", 'Э': "e", 'Ю': "yu",
+		'Я': "ya",
+	}
+
+	var result string
+	for _, char := range text {
+		if engChar, exists := translitMap[char]; exists {
+			result += engChar
+		} else {
+			result += string(char)
+		}
+	}
+	return result
+}
+
+// GenerateAirports - генерация аэропортов
 func (g *TestDataGenerator) GenerateAirports() []models.Airport {
 	return []models.Airport{
-		{Code: "SVO", Name: "Шереметьево", City: "Москва", Country: "RU"},
-		{Code: "DME", Name: "Домодедово", City: "Москва", Country: "RU"},
-		{Code: "VKO", Name: "Внуково", City: "Москва", Country: "RU"},
-		{Code: "LED", Name: "Пулково", City: "Санкт-Петербург", Country: "RU"},
-		{Code: "IST", Name: "Стамбул", City: "Стамбул", Country: "TR"},
-		{Code: "FRA", Name: "Франкфурт", City: "Франкфурт", Country: "DE"},
-		{Code: "CDG", Name: "Шарль-де-Голль", City: "Париж", Country: "FR"},
-		{Code: "LHR", Name: "Хитроу", City: "Лондон", Country: "GB"},
+		{Code: "SVO", Name: "Шереметьево", City: "Москва", Country: "Россия"},
+		{Code: "DME", Name: "Домодедово", City: "Москва", Country: "Россия"},
+		{Code: "VKO", Name: "Внуково", City: "Москва", Country: "Россия"},
+		{Code: "LED", Name: "Пулково", City: "Санкт-Петербург", Country: "Россия"},
+		{Code: "KRR", Name: "Пашковский", City: "Краснодар", Country: "Россия"},
+		{Code: "OVB", Name: "Толмачёво", City: "Новосибирск", Country: "Россия"},
+		{Code: "KHV", Name: "Новый", City: "Хабаровск", Country: "Россия"},
+		{Code: "CDG", Name: "Шарль-де-Голль", City: "Париж", Country: "Франция"},
+		{Code: "JFK", Name: "Джон Кеннеди", City: "Нью-Йорк", Country: "США"},
+		{Code: "DXB", Name: "Дубай", City: "Дубай", Country: "ОАЭ"},
 	}
 }
 
-// GenerateAirlines генерирует тестовые авиакомпании
+// GenerateAirlines - генерация авиакомпаний
 func (g *TestDataGenerator) GenerateAirlines() []models.Airline {
 	return []models.Airline{
-		{Code: "SU", Name: "Аэрофлот", Country: "RU"},
-		{Code: "S7", Name: "S7 Airlines", Country: "RU"},
-		{Code: "U6", Name: "Уральские авиалинии", Country: "RU"},
-		{Code: "TK", Name: "Turkish Airlines", Country: "TR"},
-		{Code: "LH", Name: "Lufthansa", Country: "DE"},
-		{Code: "AF", Name: "Air France", Country: "FR"},
-		{Code: "BA", Name: "British Airways", Country: "GB"},
+		{Code: "SU", Name: "Аэрофлот", Country: "Россия"},
+		{Code: "S7", Name: "S7 Airlines", Country: "Россия"},
+		{Code: "U6", Name: "Уральские авиалинии", Country: "Россия"},
+		{Code: "FV", Name: "Россия", Country: "Россия"},
+		{Code: "AF", Name: "Air France", Country: "Франция"},
+		{Code: "LH", Name: "Lufthansa", Country: "Германия"},
+		{Code: "TK", Name: "Turkish Airlines", Country: "Турция"},
+		{Code: "EY", Name: "Etihad Airways", Country: "ОАЭ"},
 	}
 }
 
-// GenerateFlights генерирует тестовые рейсы
+// GenerateFlights - генерация рейсов
 func (g *TestDataGenerator) GenerateFlights(count int, airlines []models.Airline, airports []models.Airport) []models.Flight {
-	var flights []models.Flight
-
-	flightNumbers := []string{"1234", "567", "789", "901", "345", "678", "234", "890"}
+	flights := make([]models.Flight, count)
 
 	for i := 0; i < count; i++ {
-		departureAirport := airports[rand.Intn(len(airports))]
-		arrivalAirport := airports[rand.Intn(len(airports))]
+		airline := airlines[g.random.Intn(len(airlines))]
+		departureAirport := airports[g.random.Intn(len(airports))]
+		arrivalAirport := airports[g.random.Intn(len(airports))]
 
-		// Гарантируем, что аэропорты разные
+		// Убедимся что аэропорты разные (сравниваем по коду)
 		for departureAirport.Code == arrivalAirport.Code {
-			arrivalAirport = airports[rand.Intn(len(airports))]
+			arrivalAirport = airports[g.random.Intn(len(airports))]
 		}
 
-		airline := airlines[rand.Intn(len(airlines))]
-		departureTime := time.Now().Add(time.Duration(rand.Intn(30)) * 24 * time.Hour)
-		flightDuration := time.Duration(rand.Intn(8)+1) * time.Hour
-		arrivalTime := departureTime.Add(flightDuration)
+		// Генерируем время вылета в ближайшие 30 дней
+		departureTime := time.Now().Add(time.Duration(g.random.Intn(30*24)) * time.Hour)
+		// Продолжительность полета 1-8 часов
+		duration := time.Duration(1+g.random.Intn(8)) * time.Hour
+		arrivalTime := departureTime.Add(duration)
 
-		flight := models.Flight{
-			ID:                   uint(i + 1),
-			FlightNumber:         fmt.Sprintf("%s %s", airline.Code, flightNumbers[rand.Intn(len(flightNumbers))]),
-			Airline:              airline.Name,
-			DepartureCity:        departureAirport.City,
-			DepartureAirportCode: departureAirport.Code,
-			ArrivalCity:          arrivalAirport.City,
-			ArrivalAirportCode:   arrivalAirport.Code,
-			DepartureTime:        departureTime,
-			ArrivalTime:          arrivalTime,
-			Price:                float64(rand.Intn(20000) + 5000),
-			AvailableSeats:       rand.Intn(200) + 50,
-			CreatedAt:            time.Now(),
-			UpdatedAt:            time.Now(),
+		// Цена от 2000 до 30000 рублей
+		price := 2000.0 + float64(g.random.Intn(28000))
+
+		// Доступные места от 50 до 300
+		availableSeats := 50 + g.random.Intn(250)
+
+		flights[i] = models.Flight{
+			FlightNumber:   fmt.Sprintf("%s%d", airline.Code, 1000+g.random.Intn(9000)),
+			Airline:        airline.Name, // Используем название авиакомпании
+			DepartureCity:  departureAirport.City,
+			ArrivalCity:    arrivalAirport.City,
+			DepartureTime:  departureTime,
+			ArrivalTime:    arrivalTime,
+			Price:          price,
+			AvailableSeats: availableSeats,
+			CreatedAt:      time.Now(),
+			UpdatedAt:      time.Now(),
 		}
-
-		flights = append(flights, flight)
 	}
 
 	return flights
+}
+
+// GenerateBookings - генерация бронирований с использованием реальных ID
+func (g *TestDataGenerator) GenerateBookings(count int, userIDs []uint, flightIDs []uint) []models.Booking {
+	bookings := make([]models.Booking, count)
+
+	classes := []string{"economy", "business", "first"}
+	statuses := []string{"confirmed", "pending", "cancelled"}
+	paymentStatuses := []string{"paid", "pending", "failed"}
+
+	for i := 0; i < count; i++ {
+		// Выбираем случайные ID из существующих
+		userID := userIDs[g.random.Intn(len(userIDs))]
+		flightID := flightIDs[g.random.Intn(len(flightIDs))]
+
+		seats := 1 + g.random.Intn(4) // 1-4 места
+		class := classes[g.random.Intn(len(classes))]
+		status := statuses[g.random.Intn(len(statuses))]
+		paymentStatus := paymentStatuses[g.random.Intn(len(paymentStatuses))]
+
+		// Общая сумма (можно сделать более сложную логику)
+		totalAmount := float64(seats) * (1000.0 + float64(g.random.Intn(20000)))
+
+		// Дата бронирования - случайная дата в последние 30 дней
+		bookingDate := time.Now().Add(-time.Duration(g.random.Intn(30*24)) * time.Hour)
+
+		bookings[i] = models.Booking{
+			UserID:        userID,
+			FlightID:      flightID,
+			Seats:         seats,
+			Class:         class,
+			BookingDate:   bookingDate,
+			TotalAmount:   totalAmount,
+			Status:        status,
+			PaymentStatus: paymentStatus,
+			CreatedAt:     time.Now(),
+			UpdatedAt:     time.Now(),
+		}
+	}
+
+	return bookings
 }
